0fae90cc3bc99542e617ebdcf7b9a8c5308ceada
masegovi96 <mario.segovia.gtz@outlook.com>
2025-10-14 08:44:02 -0600

    Clase del 14 de octubre de 2025

---
+++ Clase 1 - 14102025/clase1_14102025.py
@@ -0,0 +1,25 @@
+#Hola mundo
+print("Hola mundo")
+
+#Declaración de variables
+nombre = "Marcos"
+print(type(nombre)) #type() me dice el tipo de dato que es la variable
+print(nombre)
+
+edad = 29
+print(type(edad)) #type() me dice el tipo de dato que es la variable
+
+estatura = 1.64
+print(type(estatura)) #type() me dice el tipo de dato que es la variable
+
+isDespierto = True
+print(type(isDespierto)) #type() me dice el tipo de dato que es la variable
+
+nombre = "Ana"
+print(nombre)
+
+nombre = input("Ingrese su nombre completo: ")
+
+print("Hola ",nombre, ", bienvenido a la clase de Introducción a la Programación") #Concatenación de cadenas
+
+print(f"Hola {nombre}, bienvenido a la clase de Introducción a la Programación") #Interpolación de cadenas o cadena formateada
